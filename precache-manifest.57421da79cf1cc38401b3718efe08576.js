fe44ee2a1e53dd19302f1be1439642d4313a2b4e
Preeti Gupta <preetigupta510@gmail.com>
2020-08-14 21:05:44 -0700

    Updates

---
+++ precache-manifest.57421da79cf1cc38401b3718efe08576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f091cc95019ae6031a565f7d5cd9d31",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "1f65e1172957862a344a",
+    "url": "/user-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd954810780ac9239592",
+    "url": "/user-directory/static/js/2.2585bfd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-directory/static/js/2.2585bfd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f65e1172957862a344a",
+    "url": "/user-directory/static/js/main.00471ee5.chunk.js"
+  },
+  {
+    "revision": "e0c8332cf67520a1e021",
+    "url": "/user-directory/static/js/runtime-main.2b603db7.js"
+  }
+]);
